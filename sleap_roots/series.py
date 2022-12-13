47d63e2cb9eb7b554e151081369d2928bd3d8bf4
Talmo Pereira <talmo@salk.edu>
2022-12-12 18:18:00 -0800

    Fix and lint

--- sleap_roots/series.py
+++ sleap_roots/series.py
@@ -17,12 +17,21 @@ class Series:
     lateral_name: str = "lateral__nodes"
     primary_labels: Optional[sleap.Labels] = None
     lateral_labels: Optional[sleap.Labels] = None
-    
+
     @classmethod
-    def load(cls, h5_path: str, primary_name: str = "primary_multi_day", lateral_name: str = "lateral__nodes"):
-        primary_path = Path(h5_path).with_suffix(f".{primary_name}.predictions.slp").as_posix()
-        lateral_path = Path(h5_path).with_suffix(f".{lateral_name}.predictions.slp").as_posix()
-        
+    def load(
+        cls,
+        h5_path: str,
+        primary_name: str = "primary_multi_day",
+        lateral_name: str = "lateral__nodes",
+    ):
+        primary_path = (
+            Path(h5_path).with_suffix(f".{primary_name}.predictions.slp").as_posix()
+        )
+        lateral_path = (
+            Path(h5_path).with_suffix(f".{lateral_name}.predictions.slp").as_posix()
+        )
+
         return cls(
             h5_path,
             primary_name=primary_name,
@@ -30,37 +39,43 @@ class Series:
             primary_labels=sleap.load_file(primary_path),
             lateral_labels=sleap.load_file(lateral_path),
         )
-    
+
     @property
     def series_name(self) -> str:
         return Path(self.h5_path).stem
-    
+
     @property
     def video(self) -> sleap.Video:
         return self.primary_labels.video
-    
+
     def __len__(self) -> int:
         return len(self.video)
-    
-    def __getitem__(self, idx: int) ->  Tuple[sleap.LabeledFrame, sleap.LabeledFrame]:
+
+    def __getitem__(self, idx: int) -> Tuple[sleap.LabeledFrame, sleap.LabeledFrame]:
         return self.get_frame(idx)
-    
+
     def __iter__(self):
         for i in range(len(self)):
             yield self[i]
-    
-    def get_frame(self, frame_idx: int) -> Tuple[sleap.LabeledFrame, sleap.LabeledFrame]:
+
+    def get_frame(
+        self, frame_idx: int
+    ) -> Tuple[sleap.LabeledFrame, sleap.LabeledFrame]:
         """Return labeled frames for primary and lateral predictions.
-        
+
         Args:
             frame_idx: Integer frame number.
-        
+
         Returns:
             Tuple of (primary_lf, lateral_lf) corresponding to the
             sleap.LabeledFrames from each set of predictions on the same frame.
         """
-        lf_primary = self.primary_labels.find(self.primary_labels.video, frame_idx, return_new=True)[0]
-        lf_lateral = self.lateral_labels.find(self.lateral_labels.video, frame_idx, return_new=True)[0]
+        lf_primary = self.primary_labels.find(
+            self.primary_labels.video, frame_idx, return_new=True
+        )[0]
+        lf_lateral = self.lateral_labels.find(
+            self.lateral_labels.video, frame_idx, return_new=True
+        )[0]
         return lf_primary, lf_lateral
 
     def plot(self, frame_idx: int, scale: float = 1.0, **kwargs):
@@ -70,12 +85,12 @@ class Series:
         sleap.nn.viz.plot_instances(lateral_lf.instances, cmap=["g"], **kwargs)
 
 
-def find_all_series(data_folders: list[str]) -> list[str]:
+def find_all_series(data_folders: List[str]) -> List[str]:
     """Find all .h5 series from a list of folders.
-    
+
     Args:
         data_folders: List of paths to folders containing .h5 series.
-    
+
     Returns:
         A list of filenames to .h5 series.
     """
