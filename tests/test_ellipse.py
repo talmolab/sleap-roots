886b315b98371ec33b299d8cdeae9c3c53047a37
linwang9926 <84240210+linwang9926@users.noreply.github.com>
2022-12-14 13:44:27 -0800

    Implement basic ellipse fitter (#7)
    * Add scikit-image to dependencies
    
    * Add base ellipse fitter and tests
    
    Co-authored-by: linwang9926 <linwang9926@users.noreply.github.com>

---
+++ tests/test_ellipse.py
@@ -0,0 +1,26 @@
+from sleap_roots import Series
+import numpy as np
+from sleap_roots.ellipse import fit_ellipse
+
+
+def test_get_ellipse(canola_h5):
+    series = Series.load(
+        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
+    )
+    primary, lateral = series[0]
+
+    pts = primary.numpy()
+    a, b, ratio = fit_ellipse(pts)
+    np.testing.assert_almost_equal(a, 733.3038028507555)
+    np.testing.assert_almost_equal(b, 146.47723651978848)
+    np.testing.assert_almost_equal(ratio, 0.19974972985323525)
+
+    a, b, ratio = fit_ellipse(np.array([[1, 2], [np.nan, np.nan], [np.nan, np.nan]]))
+    assert np.isnan(a)
+    assert np.isnan(b)
+    assert np.isnan(ratio)
+
+    a, b, ratio = fit_ellipse(np.array([[np.nan, np.nan], [np.nan, np.nan]]))
+    assert np.isnan(a)
+    assert np.isnan(b)
+    assert np.isnan(ratio)
