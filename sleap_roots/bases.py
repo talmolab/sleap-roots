7ce90eee62e8968d289aa936479c28304bc457b3
eberrigan <106755962+eberrigan@users.noreply.github.com>
2022-12-14 13:05:32 -0800

    Added bases functions (#6)
    * Added bases function
    
    * Add bases module trait functions and tests
    
    * Fix lint
    
    * Add vscode config folder to gitignore
    
    Co-authored-by: Elizabeth Berrigan <eberrigan@salk.edu>

--- sleap_roots/bases.py
+++ sleap_roots/bases.py
@@ -1,3 +1,32 @@
 """Trait calculations that rely on bases (i.e., dicot-only)."""
 
-# test
+import numpy as np
+
+
+def get_bases(pts: np.ndarray) -> np.ndarray:
+    """Return bases (r1) from each lateral root.
+
+    Args:
+        pts: Root landmarks as array of shape (instances, nodes, 2)
+
+    Returns:
+        Array of bases (instances, (x, y)).
+    """
+    # (instances, 2)
+    base_pts = pts[:, 0]
+    return base_pts
+
+
+def get_root_lengths(pts: np.ndarray) -> np.ndarray:
+    """Return root lengths for all roots in a frame.
+
+    Args:
+        pts: Root landmarks as array of shape (instances, nodes, 2).
+
+    Returns:
+        Array of root lengths of shape (instances,).
+    """
+    segment_diffs = np.diff(pts, axis=1)
+    segment_lengths = np.linalg.norm(segment_diffs, axis=-1)
+    total_lengths = np.nansum(segment_lengths, axis=-1)
+    return total_lengths
