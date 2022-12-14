391481ec4e4c8c7155ecf7b7f7e8b0b098f8e246
Elizabeth Berrigan <eberrigan@salk.edu>
2022-12-14 12:53:16 -0800

    Add bases module trait functions and tests

--- sleap_roots/bases.py
+++ sleap_roots/bases.py
@@ -1,8 +1,11 @@
 """Trait calculations that rely on bases (i.e., dicot-only)."""
 
+import numpy as np
 
-def get_bases(pts):
+
+def get_bases(pts: np.ndarray) -> np.ndarray:
     """Return bases (r1) from each lateral root.
+
     Args:
         pts: Root landmarks as array of shape (instances, nodes, 2)
 
@@ -13,3 +16,17 @@ def get_bases(pts):
     base_pts = pts[:, 0]
     return base_pts
 
+
+def get_root_lengths(pts: np.ndarray) -> np.ndarray:
+    """Return root lengths for all roots in a frame.
+
+    Args:
+        pts: Root landmarks as array of shape (instances, nodes, 2).
+
+    Returns:
+        Array of root lengths of shape (instances,).
+    """
+    segment_diffs = np.diff(pts, axis=1)
+    segment_lengths = np.linalg.norm(segment_diffs, axis=-1)
+    total_lengths = np.nansum(segment_lengths, axis=-1)
+    return total_lengths
