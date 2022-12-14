db78eec6f111ea1ca019cc5d4b9ffb3fab404282
Elizabeth Berrigan <eberrigan@salk.edu>
2022-12-14 11:56:18 -0800

    Added bases function

--- sleap_roots/bases.py
+++ sleap_roots/bases.py
@@ -1,1 +1,14 @@
 """Trait calculations that rely on bases (i.e., dicot-only)."""
+
+
+def get_bases(pts):
+    """Return bases (r1) from each lateral root.
+    Args:
+        pts: Root landmarks as array of shape (instances, nodes, 2)
+
+    Returns:
+        Array of bases (instances, (x, y)).
+    """
+    # (instances, 2)
+    base_pts = pts[:, 0]
+    return base_pts
