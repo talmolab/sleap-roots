6bf30a2d7412846e8f6d333c72e1a1ef3e24b3b2
Lin Wang <wanglin9926@gmail.com>
2022-12-16 08:19:54 -0800

    Add rice test data files and define as fixture.

--- tests/fixtures/data.py
+++ tests/fixtures/data.py
@@ -23,3 +23,27 @@ def canola_primary_slp():
 def canola_lateral_slp():
     """Path to lateral root predictions for 7 day old canola."""
     return "tests/data/canola_7do/919QDUH.lateral_3_nodes.predictions.slp"
+
+
+@pytest.fixture
+def rice_folder():
+    """Path to a folder with the predictions for 3 day old rice."""
+    return "tests/data/rice_3do"
+
+
+@pytest.fixture
+def rice_h5():
+    """Path to root image stack for 3 day old rice."""
+    return "tests/data/rice_3do/YR39SJX.h5"
+
+
+@pytest.fixture
+def rice_long_slp():
+    """Path to longest root predictions for 3 day old rice."""
+    return "tests/data/rice_3do/YR39SJX.longest_3do_6nodes.predictions.slp"
+
+
+@pytest.fixture
+def rice_main_slp():
+    """Path to main root predictions for 3 day old rice."""
+    return "tests/data/rice_3do/YR39SJX.main_3do_6nodes.predictions.slp"
