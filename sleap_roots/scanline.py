562aeb2085d1dcf1bd772806cf780c2e2087016b
Lin Wang <linwang9926@gmail.com>
2023-01-20 14:04:52 -0800

    Modify scan line functions to get location and number of intersections.

--- sleap_roots/scanline.py
+++ sleap_roots/scanline.py
@@ -5,7 +5,7 @@ import math
 from shapely import LineString
 
 
-def get_intersection_scanline(pts: np.ndarray, depth=1080, width=2048, Nline=50) -> list:
+def get_pt_scanline(pts: np.ndarray, depth=1080, width=2048, Nline=50) -> list:
     """Get intersection points of roots and scan lines.
     
     Args:
@@ -20,8 +20,8 @@ def get_intersection_scanline(pts: np.ndarray, depth=1080, width=2048, Nline=50)
     """
     # connect the points to lines using shapely
     points = list(pts)
-    # lines = MultiLineString(points)
     
+    # calculate interval between two scan lines
     Ninterval = math.ceil(depth/(Nline-1))
     
     intersection = []
@@ -32,9 +32,34 @@ def get_intersection_scanline(pts: np.ndarray, depth=1080, width=2048, Nline=50)
         #intersection = np.zeros([5,2])
         intersection_line = []
         for j in range(len(points)):
-            if line.intersects(LineString(points[j])):
-                intersection_root = line.intersection(LineString(points[j]))
-                intersection_line.append([intersection_root.x,intersection_root.y])
+            # filter out nan nodes
+            pts_j = points[j][~np.isnan(points[j]).any(axis=1)]
+            if pts_j.shape[0]>1:
+                if line.intersects(LineString(points[j])):
+                    intersection_root = line.intersection(LineString(points[j]))
+                    intersection_line.append([intersection_root.x,intersection_root.y])
     
         intersection.append(intersection_line)
     return intersection
+
+
+def get_Npt_scanline(pts: np.ndarray, depth=1080, width=2048, Nline=50) -> np.ndarray:
+    """Get number of intersection points of roots and scan lines.
+    
+    Args:
+        pts: Numpy array of points of shape (instances, nodes, 2).
+        depth: the depth of cylinder, or number of rows of the image.
+        width: the width of cylinder, or number of columns of the image.
+        Nline: number of scan lines.
+        
+    Returns:
+        An array with shape of (#Nline,) of intersection numbers of each scan line.
+    """
+    intersection = get_pt_scanline(pts, depth, width, Nline)
+
+    Ninter = []
+    for i in range(len(intersection)):
+        Num_inter = len(intersection[i])
+        Ninter.append(Num_inter)
+    Ninter = np.array(Ninter)
+    return Ninter
---
+++ tests/test_scanline.py
@@ -0,0 +1,97 @@
+import pytest
+import numpy as np
+from sleap_roots import Series
+from sleap_roots.scanline import get_pt_scanline, get_Npt_scanline
+
+
+@pytest.fixture
+def pts_nan3():
+    return np.array(
+        [
+            [
+                [852.17755127, 216.95648193],
+                [np.nan, 472.83520508],
+                [844.45300293, np.nan],
+            ]
+        ]
+    )
+
+
+def test_get_pt_scanline(canola_h5):
+    series = Series.load(
+        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    depth = 1080
+    width = 2048
+    Nline = 50
+    intersection = get_pt_scanline(pts, depth, width, Nline)
+    assert len(intersection) == 50
+    np.testing.assert_almost_equal(intersection[10], [[1146.7898883311389, 253.0]], decimal=7)
+
+
+def test_get_pt_scanline_rice(rice_h5):
+    series = Series.load(
+        rice_h5, primary_name="main_3do_6nodes", lateral_name="longest_3do_6nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+
+    depth = 1080
+    width = 2048
+    Nline = 50
+
+    intersection = get_pt_scanline(pts, depth, width, Nline)
+    assert len(intersection) == 50
+    np.testing.assert_almost_equal(intersection[14], [[811.6129907162684, 345.0], [850.4184814416584, 345.0]], decimal=7)
+
+
+def test_scanline_nan(pts_nan3):
+    pts = pts_nan3
+    depth = 1080
+    width = 2048
+    Nline = 50
+    intersection = get_pt_scanline(pts, depth, width, Nline)
+    assert len(intersection) == 50
+    np.testing.assert_almost_equal(intersection[1], [], decimal=7)
+
+
+def test_get_Npt_scanline(canola_h5):
+    series = Series.load(
+        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    depth = 1080
+    width = 2048
+    Nline = 50
+    Ninter = get_Npt_scanline(pts, depth, width, Nline)
+    assert Ninter.shape == (50,)
+    np.testing.assert_equal(Ninter[14], 1)
+
+
+def test_get_Npt_scanline_rice(rice_h5):
+    series = Series.load(
+        rice_h5, primary_name="main_3do_6nodes", lateral_name="longest_3do_6nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+
+    depth = 1080
+    width = 2048
+    Nline = 50
+
+    Ninter = get_Npt_scanline(pts, depth, width, Nline)
+    assert Ninter.shape == (50,)
+    np.testing.assert_equal(Ninter[14], 2)
+
+
+def test_Npt_scanline_nan(pts_nan3):
+    pts = pts_nan3
+    depth = 1080
+    width = 2048
+    Nline = 50
+    Ninter = get_Npt_scanline(pts, depth, width, Nline)
+    assert len(Ninter) == 50
+    np.testing.assert_equal(Ninter[14], 0)
