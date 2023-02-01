d3dd40b75d7a5f19425d0026cf03e69a0d463899
Lin Wang <linwang9926@gmail.com>
2023-02-01 08:24:41 -0800

    Add get bbox function and network width to height ratio function.

--- sleap_roots/networklength.py
+++ sleap_roots/networklength.py
@@ -3,6 +3,52 @@
 import numpy as np
 from shapely import LineString, Polygon
 from sleap_roots.bases import get_root_lengths
+from typing import Tuple
+
+
+def get_bbox(pts: np.ndarray) -> Tuple[float, float, float, float]:
+    """Return the bounding box of all landmarks.
+
+    Args:
+        pts: Root landmarks as array of shape (..., 2).
+
+    Returns:
+        Tuple of four parameters in bounding box:
+        left_x, the x axis value of left side
+        top_y, the y axis value of top side
+        width, the width of the bounding box
+        height, the height of bounding box.
+    """
+    # reshape to (# instance, 2) and filter out NaNs.
+    pts2 = pts.reshape(-1, 2)
+    pts2 = pts2[~(np.isnan(pts2).any(axis=-1))]
+
+    # get the bounding box
+    left_x, top_y = np.min(pts2[:, 0]), np.min(pts2[:, 1])
+    width, height = np.max(pts2[:, 0]) - np.min(pts2[:, 0]), np.max(
+        pts2[:, 1]
+    ) - np.min(pts2[:, 1])
+    bbox = (left_x, top_y, width, height)
+    return bbox
+
+
+def get_network_width_depth_ratio(pts: np.ndarray) -> float:
+    """Return width to depth ratio of bounding box for root network.
+
+    Args:
+        pts: Root landmarks as array of shape (..., 2).
+
+    Returns:
+        float of bounding box width to depth ratio of root network.
+    """
+    # get the bounding box
+    bbox = get_bbox(pts)
+    width, height = bbox[2], bbox[3]
+    if width > 0 and height > 0:
+        ratio = width / height
+        return ratio
+    else:
+        return np.nan
 
 
 def get_network_distribution(pts: np.ndarray, fraction: float = 2 / 3) -> float:
@@ -15,16 +61,9 @@ def get_network_distribution(pts: np.ndarray, fraction: float = 2 / 3) -> float:
     Returns:
         float of the root network length in the lower fraction of the plant.
     """
-    # reshape to (# instance, 2) and filter out NaNs.
-    pts2 = pts.reshape(-1, 2)
-    pts2 = pts2[~(np.isnan(pts2).any(axis=-1))]
-
     # get the bounding box
-    left_x, top_y = np.min(pts2[:, 0]), np.min(pts2[:, 1])
-    width, height = np.max(pts2[:, 0]) - np.min(pts2[:, 0]), np.max(
-        pts2[:, 1]
-    ) - np.min(pts2[:, 1])
-    bbox = (left_x, top_y, width, height)
+    bbox = get_bbox(pts)
+    left_x, top_y, width, height = bbox[0], bbox[1], bbox[2], bbox[3]
 
     # get the bounding box of the lower fraction
     lower_height = bbox[3] * fraction
@@ -61,6 +100,22 @@ def get_network_distribution(pts: np.ndarray, fraction: float = 2 / 3) -> float:
                 )
 
     return root_length
+
+
+def get_network_length(pts: np.ndarray) -> float:
+    """Return all primary or lateral root length one frame.
+
+    Args:
+        pts: Root landmarks as array of shape (..., 2).
+
+    Returns:
+        float of primary or lateral root network length.
+    """
+    if np.sum(get_root_lengths(pts)) > 0:
+        length = np.sum(get_root_lengths(pts))
+        return length
+    else:
+        return 0
 
 
 def get_network_distribution_ratio(pts: np.ndarray, fraction: float = 2 / 3) -> float:
--- tests/test_networklength.py
+++ tests/test_networklength.py
@@ -1,8 +1,11 @@
 import pytest
 import numpy as np
 from sleap_roots import Series
+from sleap_roots.networklength import get_bbox
 from sleap_roots.networklength import get_network_distribution
 from sleap_roots.networklength import get_network_distribution_ratio
+from sleap_roots.networklength import get_network_length
+from sleap_roots.networklength import get_network_width_depth_ratio
 
 
 @pytest.fixture
@@ -16,6 +19,64 @@ def pts_nan3():
             ]
         ]
     )
+
+
+def test_get_bbox(canola_h5):
+    series = Series.load(
+        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    bbox = get_bbox(pts)
+    np.testing.assert_almost_equal(
+        bbox, [1016.7844238, 144.4191589, 192.1080322, 876.5622253], decimal=7
+    )
+
+
+def test_get_bbox_rice(rice_h5):
+    series = Series.load(
+        rice_h5, primary_name="main_3do_6nodes", lateral_name="longest_3do_6nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    bbox = get_bbox(pts)
+    np.testing.assert_almost_equal(
+        bbox, [796.2611694, 248.6078033, 64.3410645, 715.6949921], decimal=7
+    )
+
+
+def test_get_bbox_nan(pts_nan3):
+    pts = pts_nan3
+    bbox = get_bbox(pts)
+    np.testing.assert_almost_equal(
+        bbox, [852.1775513, 216.9564819, 0.0, 0.0], decimal=7
+    )
+
+
+def test_get_network_width_depth_ratio(canola_h5):
+    series = Series.load(
+        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    ratio = get_network_width_depth_ratio(pts)
+    np.testing.assert_almost_equal(ratio, 0.2191607471467916, decimal=7)
+
+
+def test_get_network_width_depth_ratio_rice(rice_h5):
+    series = Series.load(
+        rice_h5, primary_name="main_3do_6nodes", lateral_name="longest_3do_6nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    ratio = get_network_width_depth_ratio(pts)
+    np.testing.assert_almost_equal(ratio, 0.0899001182996162, decimal=7)
+
+
+def test_get_network_width_depth_ratio_nan(pts_nan3):
+    pts = pts_nan3
+    ratio = get_network_width_depth_ratio(pts)
+    np.testing.assert_almost_equal(ratio, np.nan, decimal=7)
 
 
 def test_get_network_distribution(canola_h5):
@@ -45,6 +106,32 @@ def test_get_network_distribution_nan(pts_nan3):
     fraction = 2 / 3
     root_length = get_network_distribution(pts, fraction)
     np.testing.assert_almost_equal(root_length, 0, decimal=7)
+
+
+def test_get_network_length(canola_h5):
+    series = Series.load(
+        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    length = get_network_length(pts)
+    np.testing.assert_almost_equal(length, 971.0504174567843, decimal=7)
+
+
+def test_get_network_length_rice(rice_h5):
+    series = Series.load(
+        rice_h5, primary_name="main_3do_6nodes", lateral_name="longest_3do_6nodes"
+    )
+    primary, lateral = series[0]
+    pts = primary.numpy()
+    length = get_network_length(pts)
+    np.testing.assert_almost_equal(length, 798.5726441151357, decimal=7)
+
+
+def test_get_network_length_nan(pts_nan3):
+    pts = pts_nan3
+    length = get_network_length(pts)
+    np.testing.assert_almost_equal(length, 0, decimal=7)
 
 
 def test_get_network_distribution_ratio(canola_h5):
