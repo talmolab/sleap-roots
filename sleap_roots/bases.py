4b529ae55cf94325f9da1331bba9f452d8f8e0f8
Elizabeth Berrigan <berri104@gmail.com>
2023-07-26 17:26:08 -0700

    Refactor `get_base_ys`, `get_base_length`, and `base_ct_density` to use graph and add comments. Delete duplicate `primary_depth`.

--- sleap_roots/bases.py
+++ sleap_roots/bases.py
@@ -114,107 +114,196 @@ def get_lateral_count(pts: np.ndarray):
 
 
 def get_base_xs(pts: np.ndarray, monocots: bool = False) -> np.ndarray:
-    """Get x coordinations of base points.
+    """
+    Get x coordinates of the base of each lateral root.
 
     Args:
-        pts: root landmarks as array of shape (instance, point, 2)
+        pts: root landmarks as array of shape (instances, point, 2)
+        or bases (instances, 2)
         monocots: Boolean value, where false is dicot (default), true is rice.
 
     Return:
-        An array of bases in x axis (instance,).
+        An array of the x-coordinates of bases (instance,).
     """
+    # If the input is a single number (float or integer), return np.nan
+    if isinstance(pts, (np.floating, float, np.integer, int)):
+        return np.nan
+
+    # If the input array doesn't have 2 or 3 dimensions, raise an error
     if pts.ndim not in (2, 3):
         raise ValueError(
             "Input array must be 2-dimensional (n_bases, 2) or "
             "3-dimensional (n_roots, n_nodes, 2)."
         )
 
+    # If the input array has 3 dimensions, calculate the base points,
+    # otherwise, assume the input array already contains the base points
     if pts.ndim == 3:
         _base_pts = get_bases(
             pts, monocots
-        )  # Assuming get_tips returns an array of shape (instance, 2)
+        )  # Assuming get_bases returns an array of shape (instance, 2)
     else:
         _base_pts = pts
 
+    # If _base_pts is a single number (float or integer), return np.nan
+    if isinstance(_base_pts, (np.floating, float, np.integer, int)):
+        return np.nan
+
+    # If the base points array doesn't have exactly 2 dimensions or
+    # the second dimension is not of size 2, raise an error
     if _base_pts.ndim != 2 or _base_pts.shape[1] != 2:
         raise ValueError(
             "Array of base points must be 2-dimensional with shape (instance, 2)."
         )
 
-    if isinstance(_base_pts, (np.floating, float, np.integer, int)):
-        return np.nan
+    # If everything is fine, extract and return the x-coordinates of the base points
     else:
         base_xs = _base_pts[:, 0]
         return base_xs
 
 
 def get_base_ys(pts: np.ndarray, monocots: bool = False) -> np.ndarray:
-    """Get y coordinations of base points.
+    """Get y coordinates of the base of each lateral root.
 
     Args:
-        pts: root landmarks as array of shape (instance, point, 2).
+        pts: root landmarks as array of shape (instances, point, 2)
+        or bases (instances, 2)
         monocots: Boolean value, where false is dicot (default), true is rice.
 
     Return:
-        An array of bases in y axis (instance,).
+        An array of the y-coordinates of bases (instance,).
     """
-    _base_pts = get_bases(pts, monocots)
+    # If the input is a single number (float or integer), return np.nan
+    if isinstance(pts, (np.floating, float, np.integer, int)):
+        return np.nan
+
+    # If the input array doesn't have 2 or 3 dimensions, raise an error
+    if pts.ndim not in (2, 3):
+        raise ValueError(
+            "Input array must be 2-dimensional (n_bases, 2) or "
+            "3-dimensional (n_roots, n_nodes, 2)."
+        )
+
+    # If the input array has 3 dimensions, calculate the base points,
+    # otherwise, assume the input array already contains the base points
+    if pts.ndim == 3:
+        _base_pts = get_bases(
+            pts, monocots
+        )  # Assuming get_bases returns an array of shape (instance, 2)
+    else:
+        _base_pts = pts
+
+    # If _base_pts is a single number (float or integer), return np.nan
     if isinstance(_base_pts, (np.floating, float, np.integer, int)):
         return np.nan
+
+    # If the base points array doesn't have exactly 2 dimensions or
+    # the second dimension is not of size 2, raise an error
+    if _base_pts.ndim != 2 or _base_pts.shape[1] != 2:
+        raise ValueError(
+            "Array of base points must be 2-dimensional with shape (instance, 2)."
+        )
     else:
         base_ys = _base_pts[:, 1]
         return base_ys
 
 
 def get_base_length(pts: np.ndarray, monocots: bool = False):
-    """Get lateral roots top and deepest bases distance in y axis.
+    """Get the distance on the y-axis from the top lateral root base to the
+        bottom lateral root base.
 
     Args:
-        pts: lateral root landmarks as array of shape (instance, point, 2)
+        pts: root landmarks as array of shape (instances, point, 2)
+        or base_ys (instances).
         monocots: Boolean value, where false is dicot (default), true is rice.
 
     Return:
-        Top and deepest bases distance y-axis.
+        The distance between the top base y-coordinate and the deepest
+        base y-coordinate.
     """
-    base_ys = get_base_ys(pts, monocots)
+    # If the input is a single number (float or integer), return np.nan
+    if isinstance(pts, (np.floating, float, np.integer, int)):
+        return np.nan
+
+    if pts.ndim not in (1, 3):
+        raise ValueError(
+            "Input array must be 1-dimensional (n_base_ys) or "
+            "3-dimensional (n_roots, n_nodes, 2)."
+        )
+
+    if pts.ndim == 3:
+        base_ys = get_base_ys(
+            pts, monocots
+        )  # Assuming get_base_ys returns an array of shape (instances)
+    else:
+        base_ys = pts
+
+    # If base_ys is a single number (float or integer), return np.nan
+    if isinstance(base_ys, (np.floating, float, np.integer, int)):
+        return np.nan
+
+    if base_ys.ndim != 1:
+        raise ValueError(
+            "Array of base y-coordinates must be 1-dimensional with shape (instances)."
+        )
+
     base_length = np.nanmax(base_ys) - np.nanmin(base_ys)
     return base_length
 
 
 def get_base_ct_density(primary_pts, lateral_pts, monocots: bool = False):
-    """Get number of base points to maximum primary root length.
+    """
+    Get a ratio of the number of base points to maximum primary root length.
 
     Args:
-        primary_pts: primary root points
-        lateral_pts: lateral root points
+        primary_pts: primary root points of shape (instances, nodes, 2)
+        or primary root lengths of shape (instances).
+        lateral_pts: lateral root points of shape (instances, nodes, 2)
+        or base points of lateral roots of shape (instances, 2).
         monocots: Boolean value, where false is dicot (default), true is rice.
 
     Return:
         Scalar of base count density.
     """
-    # get number of base points of lateral roots
-    _base_pts = get_bases(lateral_pts, monocots)
+    # If the inputs are single numbers (float or integer), return np.nan
+    if isinstance(lateral_pts, (np.floating, float, np.integer, int)) or isinstance(
+        primary_pts, (np.floating, float, np.integer, int)
+    ):
+        return np.nan
+
+    # If the input array has 3 dimensions, calculate the base points,
+    # otherwise, assume the input array already contains the base points
+    if lateral_pts.ndim == 3:
+        _base_pts = get_bases(
+            lateral_pts, monocots
+        )  # Assuming get_bases returns an array of shape (instances, 2)
+    else:
+        _base_pts = lateral_pts
+
+    # If _base_pts is a single number (float or integer), return np.nan
     if isinstance(_base_pts, (np.floating, float, np.integer, int)):
         return np.nan
-    else:
-        base_ct = len(_base_pts[~np.isnan(_base_pts[:, 0])])
+
+    # get number of base points of lateral roots
+    base_ct = len(_base_pts[~np.isnan(_base_pts[:, 0])])
+
+    if primary_pts.ndim == 3:
         # get primary root length
-        lengths_primary = get_root_lengths(primary_pts)
-        base_ct_density = base_ct / np.nanmax(lengths_primary)
-        return base_ct_density
+        lengths_primary = get_root_lengths(
+            primary_pts
+        )  # Assuming get_root_lengths returns an array of shape (instances)
+    else:
+        # Assuming primary_pts is an array of shape (instances)
+        lengths_primary = primary_pts
 
+    # Handle case where primary lengths are all NaN or empty to avoid division by zero
+    max_length = np.nanmax(lengths_primary)
+    if np.isnan(max_length):
+        return np.nan
 
-def get_primary_depth(primary_pts):
-    """Get primary root tip depth.
-
-    Args:
-        primary_pts: primary root points.
-
-    Return:
-        Scalar of primary root tip depth.
-    """
-    primary_depth = np.nanmax(primary_pts[:, :, 1])
-    return primary_depth
+    # get base_ct_density
+    base_ct_density = base_ct / max_length
+    return base_ct_density
 
 
 def get_base_length_ratio(primary_pts: np.ndarray, lateral_pts: np.ndarray):
