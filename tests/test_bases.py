1dbc7c4050eb42978490ebb48e6ca7fc879fbd77
Lin Wang <linwang9926@gmail.com>
2023-08-03 14:52:31 -0700

    Remove lengths-related functions from test_bases.

--- tests/test_bases.py
+++ tests/test_bases.py
@@ -2,17 +2,13 @@ from sleap_roots.bases import (
     get_bases,
     get_base_ct_density,
     get_base_tip_dist,
-    get_grav_index,
     get_lateral_count,
-    get_root_lengths,
-    get_root_lengths_max,
     get_base_xs,
     get_base_ys,
     get_base_length,
     get_base_length_ratio,
     get_root_pair_widths_projections,
 )
-from sleap_roots.points import get_lateral_pts, get_primary_pts
 from sleap_roots import Series
 import numpy as np
 import pytest
@@ -203,70 +199,6 @@ def test_get_base_tip_dist_no_roots(pts_no_roots):
     distance = get_base_tip_dist(pts=pts_no_roots)
     assert distance.shape == (2,)
     np.testing.assert_almost_equal(distance, [np.nan, np.nan], decimal=7)
-
-
-# test get_grav_index function
-def test_get_grav_index(canola_h5):
-    series = Series.load(
-        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
-    )
-    primary_pts = get_primary_pts(plant=series, frame=0)
-    grav_index = get_grav_index(pts=primary_pts)
-    np.testing.assert_almost_equal(grav_index, 0.08898137324716636)
-
-
-def test_get_root_lengths(canola_h5):
-    series = Series.load(
-        canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
-    )
-    primary, lateral = series[0]
-    pts = primary.numpy()
-    assert pts.shape == (1, 6, 2)
-
-    root_lengths = get_root_lengths(pts)
-    assert root_lengths.shape == (1,)
-    np.testing.assert_array_almost_equal(root_lengths, [971.050417])
-
-    pts = lateral.numpy()
-    assert pts.shape == (5, 3, 2)
-
-    root_lengths = get_root_lengths(pts)
-    assert root_lengths.shape == (5,)
-    np.testing.assert_array_almost_equal(
-        root_lengths, [20.129579, 62.782368, 80.268003, 34.925591, 3.89724]
-    )
-
-
-def test_get_root_lengths_no_roots(pts_no_bases):
-    root_lengths = get_root_lengths(pts_no_bases)
-    assert root_lengths.shape == (2,)
-    np.testing.assert_array_almost_equal(root_lengths, np.array([np.nan, np.nan]))
-
-
-def test_get_root_lengths_one_point(pts_one_base):
-    root_lengths = get_root_lengths(pts_one_base)
-    assert root_lengths.shape == (2,)
-    np.testing.assert_array_almost_equal(
-        root_lengths, np.array([2.82842712475, np.nan])
-    )
-
-
-# test get_root_lengths_max function with lengths_normal
-def test_get_root_lengths_max_normal(lengths_normal):
-    max_length = get_root_lengths_max(lengths_normal)
-    np.testing.assert_array_almost_equal(max_length, 329.4)
-
-
-# test get_root_lengths_max function with lengths_with_nan
-def test_get_root_lengths_max_with_nan(lengths_with_nan):
-    max_length = get_root_lengths_max(lengths_with_nan)
-    np.testing.assert_array_almost_equal(max_length, 329.4)
-
-
-# test get_root_lengths_max function with lengths_all_nan
-def test_get_root_lengths_max_all_nan(lengths_all_nan):
-    max_length = get_root_lengths_max(lengths_all_nan)
-    np.testing.assert_array_almost_equal(max_length, np.nan)
 
 
 # test get_lateral_count function with canola
