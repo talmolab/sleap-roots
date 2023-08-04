e676d0825ee34537581072eed2e32b71e9d9b8e0
Lin Wang <linwang9926@gmail.com>
2023-08-04 14:43:10 -0700

    Test points function (`get_all_pts_array`).

--- tests/test_points.py
+++ tests/test_points.py
@@ -1,119 +1,8 @@
-import numpy as np
-import pytest
 from sleap_roots import Series
+from sleap_roots.lengths import get_max_length_pts, get_root_lengths
 from sleap_roots.points import (
     get_all_pts_array,
 )
-
-
-@pytest.fixture
-def pts_nan2():
-    return np.array(
-        [
-            [
-                [852.17755127, 216.95648193],
-                [np.nan, 472.83520508],
-                [844.45300293, 472.83520508],
-                [837.03405762, 588.5123291],
-                [828.87963867, 692.72009277],
-                [816.71142578, 808.12585449],
-            ]
-        ]
-    )
-
-
-@pytest.fixture
-def pts_nan3():
-    return np.array(
-        [
-            [
-                [852.17755127, 216.95648193],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [837.03405762, 588.5123291],
-                [828.87963867, 692.72009277],
-                [816.71142578, 808.12585449],
-            ]
-        ]
-    )
-
-
-@pytest.fixture
-def pts_nan6():
-    return np.array(
-        [
-            [
-                [852.17755127, 216.95648193],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [837.03405762, 588.5123291],
-                [828.87963867, 692.72009277],
-                [np.nan, np.nan],
-            ]
-        ]
-    )
-
-
-@pytest.fixture
-def pts_nanall():
-    return np.array(
-        [
-            [
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-            ]
-        ]
-    )
-
-
-@pytest.fixture
-def pts_nan32():
-    return np.array(
-        [
-            [
-                [852.17755127, 216.95648193],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [837.03405762, 588.5123291],
-                [828.87963867, 692.72009277],
-                [816.71142578, 808.12585449],
-            ],
-            [
-                [852.17755127, 216.95648193],
-                [np.nan, np.nan],
-                [844.45300293, 472.83520508],
-                [837.03405762, 588.5123291],
-                [828.87963867, 692.72009277],
-                [816.71142578, 808.12585449],
-            ],
-        ]
-    )
-
-
-@pytest.fixture
-def pts_nan32_5node():
-    return np.array(
-        [
-            [
-                [852.17755127, 216.95648193],
-                [np.nan, np.nan],
-                [np.nan, np.nan],
-                [828.87963867, 692.72009277],
-                [816.71142578, 808.12585449],
-            ],
-            [
-                [852.17755127, 216.95648193],
-                [np.nan, np.nan],
-                [837.03405762, 588.5123291],
-                [828.87963867, 692.72009277],
-                [816.71142578, 808.12585449],
-            ],
-        ]
-    )
 
 
 # test get_all_pts_array function
@@ -121,5 +10,32 @@ def test_get_all_pts_array(canola_h5):
     plant = Series.load(
         canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
     )
-    pts_all_array = get_all_pts_array(plant=plant, frame=0, monocots=False)
+    primary, lateral = plant[0]
+    primary_pts = primary.numpy()
+    # get primary length
+    primary_max_length_pts = get_max_length_pts(primary_pts)
+    # get lateral_lengths
+    lateral_pts = lateral.numpy()
+    monocots = False
+    pts_all_array = get_all_pts_array(
+        primary_max_length_pts, lateral_pts, monocots=monocots
+    )
     assert pts_all_array.shape[0] == 21
+
+
+# test get_all_pts_array function
+def test_get_all_pts_array_rice(rice_h5):
+    plant = Series.load(
+        rice_h5, primary_name="longest_3do_6nodes", lateral_name="main_3do_6nodes"
+    )
+    primary, lateral = plant[0]
+    primary_pts = primary.numpy()
+    # get primary length
+    primary_max_length_pts = get_max_length_pts(primary_pts)
+    # get lateral_lengths
+    lateral_pts = lateral.numpy()
+    monocots = True
+    pts_all_array = get_all_pts_array(
+        primary_max_length_pts, lateral_pts, monocots=monocots
+    )
+    assert pts_all_array.shape[0] == 12
