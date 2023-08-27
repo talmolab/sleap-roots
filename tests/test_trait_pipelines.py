0c42ecbf14b29311765ba7def71ff44fa56ad320
Elizabeth Berrigan <berri104@gmail.com>
2023-08-26 19:05:27 -0700

    Add test for `OlderMonocot_pipeline`

--- tests/test_trait_pipelines.py
+++ tests/test_trait_pipelines.py
@@ -20,21 +20,10 @@ def test_dicot_pipeline(canola_h5, soy_h5):
     assert all_traits.shape == (2, 1018)
 
 
-def test_OlderMonocot_pipeline(canola_h5, soy_h5):
-    rice_10DAG = Series.load(
-        rice_main_10do_h5,
-        primary_name="primary_multi_day",
-        lateral_name="lateral_3_nodes",
-    )
-    soy = Series.load(
-        soy_h5, primary_name="primary_multi_day", lateral_name="lateral__nodes"
-    )
+def test_OlderMonocot_pipeline(rice_main_10do_h5):
+    rice = Series.load(rice_main_10do_h5, ["main_10do_6nodes"])[0]
 
     pipeline = OlderMonocotPipeline()
-    canola_traits = pipeline.compute_plant_traits(canola)
-    soy_traits = pipeline.compute_plant_traits(soy)
-    all_traits = pipeline.compute_batch_traits([canola, soy])
+    rice_10dag_traits = pipeline.compute_plant_traits(rice)
 
-    assert canola_traits.shape == (72, 115)
-    assert soy_traits.shape == (72, 115)
-    assert all_traits.shape == (2, 1018)
+    assert rice_10dag_traits.shape == (72, 115)
