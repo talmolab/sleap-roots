5556231487d0a572b0d42598a4a68e2f0242f6d5
Elizabeth Berrigan <berri104@gmail.com>
2023-09-04 18:25:23 -0700

    Require at least 5 non-collinear points to fit an ellipse

--- sleap_roots/ellipse.py
+++ sleap_roots/ellipse.py
@@ -12,27 +12,55 @@ def fit_ellipse(pts: np.ndarray) -> Tuple[float, float, float]:
         pts: Root landmarks as array of shape (..., 2).
 
     Returns:
-        A tuple of (a, b, ratio) containing the semi-major axis length, semi-minor axis
-        length, and the ratio of the major to minor lengths.
+        A tuple of (a, b, ratio) containing the semi-major axis length,
+        semi-minor axis length, and the ratio of the major to minor lengths.
 
         If the ellipse fitting fails, NaNs are returned.
     """
-    # Filter out NaNs.
+    # Reshape the input array and filter out rows containing NaNs
     pts = pts.reshape(-1, 2)
     pts = pts[~(np.isnan(pts).all(axis=-1))]
 
-    if len(pts) == 0:
+    # Check for a minimum number of points to fit an ellipse
+    if len(pts) < 5:
         return np.nan, np.nan, np.nan
 
+    # Initialize the ellipse model
     ell = EllipseModel()
-    success = ell.estimate(pts)
 
+    # Try to estimate the ellipse parameters
+    try:
+        success = ell.estimate(pts)
+    except TypeError as e:
+        # Log the error and problematic points if estimation fails
+        print(f"An error occurred: {e}")
+        print(f"Problematic pts array: {pts}")
+        return np.nan, np.nan, np.nan
+
+    # Check if the estimation was successful
     if success:
+        # Extract the ellipse parameters.
         xc, yc, a_f, b_f, theta = ell.params
+
+        # Check for complex numbers in the parameters.
+        if np.iscomplex([xc, yc, a_f, b_f, theta]).any():
+            print("Complex numbers detected.")
+            return np.nan, np.nan, np.nan
+
+        # Check for invalid (zero or NaN) major or minor axes.
+        if np.isnan(a_f) or np.isnan(b_f) or a_f == 0 or b_f == 0:
+            return np.nan, np.nan, np.nan
+
+        # Ensure a_f is the semi-major axis and b_f is the semi-minor axis.
         a_f, b_f = np.maximum(a_f, b_f), np.minimum(a_f, b_f)
+
+        # Calculate the ratio of the major to minor axis.
         ratio_ba_f = a_f / b_f
+
         return a_f, b_f, ratio_ba_f
+
     else:
+        # Return NaNs if the ellipse fitting was not successful.
         return np.nan, np.nan, np.nan
 
 
