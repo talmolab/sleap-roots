dbf82cd6d086bb7816107b16c1dc54c08eb36bc5
eberrigan <106755962+eberrigan@users.noreply.github.com>
2023-09-04 19:11:38 -0700

    Add checks and tests to ellipse fitter (#59)
    * Simplify `get_base_xs` to only allow input from `get_bases`
    
    * Simplify `get_tip_xs`
    
    * Simplify `get_base_ct_density`
    
    * Ensure that base tip coordinates are returned as scalars
    
    * Remove debugging code
    
    * Lint
    
    * Require at least 5 non-collinear points to fit an ellipse
    
    * Remove debugging statements
    
    ---------
    
    Co-authored-by: Talmo Pereira <talmo@salk.edu>

--- tests/test_ellipse.py
+++ tests/test_ellipse.py
@@ -8,14 +8,15 @@ from sleap_roots.ellipse import (
 )
 from sleap_roots.lengths import get_max_length_pts
 from sleap_roots.points import get_all_pts_array
+from typing import Literal
 
 
-def test_get_ellipse(canola_h5):
+def test_get_ellipse(canola_h5: Literal["tests/data/canola_7do/919QDUH.h5"]):
     series = Series.load(
         canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
     )
     primary, lateral = series[0]
-    # only test ellispe for primary root points
+    # only test ellipse for primary root points
     pts = primary.numpy()
     a, b, ratio = fit_ellipse(pts)
     np.testing.assert_almost_equal(a, 733.3038028507555, decimal=3)
@@ -33,7 +34,7 @@ def test_get_ellipse(canola_h5):
     assert np.isnan(ratio)
 
 
-def test_get_ellipse_a(canola_h5):
+def test_get_ellipse_a(canola_h5: Literal["tests/data/canola_7do/919QDUH.h5"]):
     plant = Series.load(
         canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
     )
@@ -48,7 +49,7 @@ def test_get_ellipse_a(canola_h5):
     np.testing.assert_almost_equal(ellipse_a, 398.1275346610801, decimal=3)
 
 
-def test_get_ellipse_b(canola_h5):
+def test_get_ellipse_b(canola_h5: Literal["tests/data/canola_7do/919QDUH.h5"]):
     plant = Series.load(
         canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
     )
@@ -63,7 +64,7 @@ def test_get_ellipse_b(canola_h5):
     np.testing.assert_almost_equal(ellipse_b, 115.03734180292595, decimal=3)
 
 
-def test_get_ellipse_ratio(canola_h5):
+def test_get_ellipse_ratio(canola_h5: Literal["tests/data/canola_7do/919QDUH.h5"]):
     plant = Series.load(
         canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
     )
@@ -78,7 +79,9 @@ def test_get_ellipse_ratio(canola_h5):
     np.testing.assert_almost_equal(ellipse_ratio, 3.460854783511295, decimal=3)
 
 
-def test_get_ellipse_ratio_ellipse(canola_h5):
+def test_get_ellipse_ratio_ellipse(
+    canola_h5: Literal["tests/data/canola_7do/919QDUH.h5"],
+):
     plant = Series.load(
         canola_h5, primary_name="primary_multi_day", lateral_name="lateral_3_nodes"
     )
@@ -92,3 +95,34 @@ def test_get_ellipse_ratio_ellipse(canola_h5):
     ellipse = fit_ellipse(pts_all_array)
     ellipse_ratio = get_ellipse_ratio(ellipse)
     np.testing.assert_almost_equal(ellipse_ratio, 3.460854783511295, decimal=3)
+
+
+def test_fit_ellipse():
+    # Test when pts is empty
+    pts = np.array([])
+    assert np.isnan(fit_ellipse(pts)).all()
+
+    # Test when pts has less than 5 points
+    pts = np.array([[0, 0], [1, 1], [2, 2], [3, 3]])
+    assert np.isnan(fit_ellipse(pts)).all()
+
+    # Test when pts has NaNs only
+    pts = np.array([[np.nan, np.nan], [np.nan, np.nan], [np.nan, np.nan]])
+    assert np.isnan(fit_ellipse(pts)).all()
+
+    # Test with collinear points
+    pts = np.array([[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]])
+    a, b, ratio = fit_ellipse(pts)
+    assert np.isnan(fit_ellipse(pts)).all()
+
+    # Test when pts can actually fit an ellipse
+    pts = np.array([[0, 1], [1, 0], [1, 1], [2, 1], [0.5, 0.5]])
+    a, b, ratio = fit_ellipse(pts)
+    np.testing.assert_almost_equal(a, 1.50246, decimal=3)
+    np.testing.assert_almost_equal(b, 0.57389, decimal=3)
+    np.testing.assert_almost_equal(ratio, 2.61803, decimal=3)
+
+    # Test when pts has some NaNs but enough valid points to fit an ellipse
+    pts = np.array([[0, 0], [1, 1], [2, 2], [3, 3], [np.nan, np.nan]])
+    a, b, ratio = fit_ellipse(pts)
+    assert np.isnan(fit_ellipse(pts)).all()
