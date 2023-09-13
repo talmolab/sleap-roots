8a14f61e43a29039d12cba0f77a21a06fae1d899
Elizabeth Berrigan <berri104@gmail.com>
2023-09-13 10:51:39 -0700

    Test `YoungerMonocotPipeline`

--- tests/test_trait_pipelines.py
+++ tests/test_trait_pipelines.py
@@ -1,5 +1,5 @@
-from sleap_roots.trait_pipelines import DicotPipeline
-from sleap_roots.series import Series
+from sleap_roots.trait_pipelines import DicotPipeline, YoungerMonocotPipeline
+from sleap_roots.series import Series, find_all_series
 
 
 def test_dicot_pipeline(canola_h5, soy_h5):
@@ -17,4 +17,24 @@ def test_dicot_pipeline(canola_h5, soy_h5):
 
     assert canola_traits.shape == (72, 117)
     assert soy_traits.shape == (72, 117)
+    assert all_traits.shape == (2, 1036)
+
+
+def test_younger_monocot_pipeline(rice_h5, rice_folder):
+    rice = Series.load(
+        rice_h5, primary_name="longest_3do_6nodes", lateral_name="main_3do_6nodes"
+    )
+    rice_series_all = find_all_series(rice_folder)
+    series_all = [
+        Series.load(
+            series, primary_name="longest_3do_6nodes", lateral_name="main_3do_6nodes"
+        )
+        for series in rice_series_all
+    ]
+
+    pipeline = YoungerMonocotPipeline()
+    rice_traits = pipeline.compute_plant_traits(rice)
+    all_traits = pipeline.compute_batch_traits(series_all)
+
+    assert rice_traits.shape == (72, 117)
     assert all_traits.shape == (2, 1036)
